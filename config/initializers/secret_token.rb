63214aa3d206e8fba5e77a0152fa619267613163
Johan Wallén <hulqen@gmail.com>
2012-05-30 09:05:58 +0200

    Create Rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Matplanera::Application.config.secret_token = '4833ee35ca423e71ce8676cee9b698fc5c3afdb94a96757a8ca5c4c25d2ff15bbfb92902e8b85d97b1e53f3d23a0d2c651fe510757a5855afeb08150023d5b15'
