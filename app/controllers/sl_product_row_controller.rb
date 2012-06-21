7696fd406a9c097d0b0fb57395f49aedd79f3f9e
Johan Wallén <hulqen@gmail.com>
2012-06-21 13:59:51 +0200

    Clean up auto generated files

--- app/controllers/sl_product_row_controller.rb
+++
@@ -1,7 +0,0 @@
-class SlProductRowController < ApplicationController
-  def destroy
-    recipe = Recipe.find(params[:id])
-    recipe.sl_product_rows.clear
-    redirect_to shopping_list_url
-  end
-end
