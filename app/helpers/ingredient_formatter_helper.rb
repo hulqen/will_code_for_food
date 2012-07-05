7aeb582c5c726d7da7de37aa11f51af209ab723d
Johan Wallén <hulqen@gmail.com>
2012-07-05 15:47:59 +0200

    Refine pluralization of units in recipes and shopping list

--- app/helpers/ingredient_formatter_helper.rb
+++ app/helpers/ingredient_formatter_helper.rb
@@ -5,36 +5,28 @@ module IngredientFormatterHelper
       unit_name    = product_name_and_unit[1]
       product_name = product_name_and_unit[0]
       
-      if incoming_amount == 0
-        amount     = nil
-
-      elsif unit_name == "l"
-        amount = number_to_human(incoming_amount, :units => :liquid)
-        unit_name = nil
-      elsif unit_name == "g"
-        amount = number_to_human(incoming_amount, :units => :weight)
-        unit_name = nil
+      if unit_name
+        amount_and_unit_name = human_numbers(incoming_amount, unit_name)
       else
-        amount = number_with_precision(incoming_amount, :precision => 5 , :separator => ',', :strip_insignificant_zeros => :true)
+        amount_and_unit_name = nil
       end
-
-      yield amount, unit_name, product_name
+      yield amount_and_unit_name, product_name
     end
   end
 
-  def humanize_ingredients(i)
-    i.each do |ingr|
-      if ingr.unit_name == "l"
-        amount = number_to_human(ingr.amount, :units => :liquid, :precision => 5 , :separator => ',', :strip_insignificant_zeros => :true)
-        unit_name = nil
-      elsif ingr.unit_name == "g"
-        amount = number_to_human(ingr.amount, :units => :weight, :precision => 5 , :separator => ',', :strip_insignificant_zeros => :true)
-        unit_name = nil
+  def humanize_ingredients(ingredient)
+    ingredient.each do |i|
+      if i.unit
+        amount_and_unit_name = human_numbers(i.amount, i.unit_name)
       else
-        amount = number_with_precision(ingr.amount, :precision => 5 , :separator => ',', :strip_insignificant_zeros => :true)
-        unit_name = ingr.unit_name
+        amount_and_unit_name = nil
       end
-      yield amount, unit_name, ingr.product_name
+      yield amount_and_unit_name, i.product_name
     end
   end
+
+  def human_numbers(amount, unit_name)
+    number_to_human(amount, :units => unit_name, :precision => 5 , :separator => ',', :strip_insignificant_zeros => :true)
+  end
+
 end
